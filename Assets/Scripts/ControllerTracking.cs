b745d51591ddb280460bfddd6a4571cad3d082a7
abitha <abitha@nyu.edu>
2023-05-03 18:56:18 -0400

    Working implementation of pub subfor streaming

--- Assets/Scripts/ControllerTracking.cs
+++ Assets/Scripts/ControllerTracking.cs
@@ -4,54 +4,114 @@ using AsyncIO;
 using System.Collections;
 using OVR;
 using System;
+using NetMQ.Sockets;
 
 public class ControllerTracking : MonoBehaviour
 {
     private OVRInput.Controller leftController = OVRInput.Controller.LTouch;
     private OVRInput.Controller rightController = OVRInput.Controller.RTouch;
+    private ControllerState controllerState;
 
-    private bool startRecording;
-    private ZMQClient zmqClient;
-    private ControllerState controllerState; 
 
-    [SerializeField]
-    private string tcpAddress = "tcp://10.19.216.156:5555";
+    private PublisherSocket publisher;
+    private string tcpAddress = "tcp://*:5555";
 
-    private void Start()
+    void Start()
     {
         Logger.Log("Init libs");
         ForceDotNet.Force();
-        zmqClient = new ZMQClient(tcpAddress);
         controllerState = new ControllerState(leftController, rightController);
 
+        Logger.Log("Create publisher");
+        // Create a new publisher socket and bind it to the TCP address
+        publisher = new PublisherSocket();
+        publisher.Bind(tcpAddress);
+
+        // Start publishing the Oculus controller state
+        //StartCoroutine(PublishOculusControllerState());
     }
 
-    private void Update()
-    {
+
+    void Update() {
         controllerState.UpdateState();
         string state = controllerState.ToString();
         Logger.Log(state);
-        zmqClient.SendMessageToServer(controllerState.ToString());
+        // Publish the message on the "oculus_controller" topic
+        publisher.SendMoreFrame("oculus_controller").SendFrame(state);
+
     }
 
-    private void OnDestroy()
+
+    void OnDestroy()
     {
-        Disconnect();
-        NetMQConfig.Cleanup();
+        // Dispose of the publisher socket when the script is destroyed
+        publisher?.Dispose();
     }
 
-    public void Disconnect()
-    {
-        zmqClient.Dispose();
-        zmqClient = null;
-    }
 
-    public void Connect()
-    {
-        if (zmqClient == null)
-        {
-            zmqClient = new ZMQClient(tcpAddress);
-        }
-    }
+}
 
-    }
+
+
+//public class ControllerTracking : MonoBehaviour
+//{
+//    private OVRInput.Controller leftController = OVRInput.Controller.LTouch;
+//    private OVRInput.Controller rightController = OVRInput.Controller.RTouch;
+
+//    private bool startRecording;
+//    private ZMQClient zmqClient;
+//    private ControllerState controllerState;
+
+//    [SerializeField]
+//    private string tcpAddress = "tcp://10.19.216.156:5555";
+
+//    private void Start()
+//    {
+//        Logger.Log("Init libs");
+//        ForceDotNet.Force();
+//        zmqClient = new ZMQClient(tcpAddress);
+//        controllerState = new ControllerState(leftController, rightController);
+
+//    }
+
+//    private void Update()
+//    {
+
+
+//        controllerState.UpdateState();
+//        string state = controllerState.ToString();
+//        Logger.Log(state);
+//        // TODO : move all try catch here
+//        try
+
+//        {
+//            zmqClient.SendMessageToServer(controllerState.ToString());
+//        }
+
+//        catch (Exception e) {
+//            Logger.Log(e.ToString());
+//        }
+
+//    }
+
+//    private void OnDestroy()
+//    {
+//        Disconnect();
+//        NetMQConfig.Cleanup();
+//    }
+
+//    public void Disconnect()
+//    {
+//        zmqClient.Dispose();
+//        zmqClient = null;
+//    }
+
+//    public void Connect()
+//    {
+//        if (zmqClient == null)
+//        {
+//            zmqClient = new ZMQClient(tcpAddress);
+//        }
+//    }
+
+//}
--- Assets/Scripts/ZmqClient.cs
+++ Assets/Scripts/ZmqClient.cs
@@ -59,3 +59,104 @@ public class ZMQClient
         Connect();
     }
 }
+//using UnityEngine;
+//using UnityEngine.UI;
+//using NetMQ;
+//using NetMQ.Sockets;
+//using AsyncIO;
+//using System;
+
+//public class ZMQClient
+//{
+//    private RequestSocket client;
+//    private string tcpAddress;
+
+//    public ZMQClient(string tcpAddress)
+//    {
+//        this.tcpAddress = tcpAddress;
+//        Connect();
+//    }
+
+
+//    public void SendMessageToServer(string message)
+//    {
+//        try
+//        {
+//            if (client == null || client.IsDisposed || client.Options.Linger < TimeSpan.Zero)
+//            {
+//                Logger.Log("Attempting to reconnect");
+//                Reconnect();
+//            }
+//        }
+//        catch (Exception e) {
+//            Logger.Log("Error reconnecting to server: " + e.Message);
+//            client?.Dispose();
+//            client = null;
+
+//        }
+
+//        // If no response; reconnect
+//        try
+//        {
+//            bool send_flag = client.TrySendFrame(message);
+//            if (!send_flag) {
+//                Logger.Log("Unable to send request within time frame.");
+//                throw new NetMQException("Did not send a request within the specified time frame.");
+
+
+//            }
+
+//            //client.SendFrame(message);
+
+//            string response;
+//            bool received_response_flag = client.TryReceiveFrameString(out response);
+
+//            if (!received_response_flag || response == null)
+//            {
+//                Logger.Log("Receive responsde timed out");
+//                throw new NetMQException("Did not receive a response within the specified time frame.");
+//            }
+
+//        }
+
+//        catch (Exception e)
+//        {
+
+//            Logger.Log("Full error: " + e.ToString());
+//            Logger.Log("Error connecting to server; during send receive: " + e.Message);
+//            client?.Dispose();
+//            client = null;
+//        }
+
+
+//    }
+
+//    public void Dispose()
+//    {
+//        client?.Dispose();
+//    }
+
+
+//    public void Connect()
+//    {
+//        try
+//        {
+//            if (client == null || client.IsDisposed || client.Options.Linger < TimeSpan.Zero)
+//            {
+//                client = new RequestSocket();
+//                client.Connect(tcpAddress);
+//            }
+//        }
+//        catch (Exception e)
+//        {
+//            Logger.Log("Error connecting to server: " + e.Message);
+//        }
+//    }
+
+//    public void Reconnect()
+//    {
+//        Logger.Log("Connection lost. Attempting to reconnect...");
+//        client?.Dispose();
+//        Connect();
+//    }
+//}
